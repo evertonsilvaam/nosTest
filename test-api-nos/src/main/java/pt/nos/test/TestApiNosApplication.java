3454d22b748419178aae7669564667c2e0319edd
lan_i <lan_i@LAPTOP-762Q06E1>
2023-03-05 16:59:28 +0000

    Commit inicial do projeto nos

---
+++ test-api-nos/src/main/java/pt/nos/test/TestApiNosApplication.java
@@ -0,0 +1,13 @@
+package pt.nos.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApiNosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApiNosApplication.class, args);
+	}
+
+}
---
+++ test-api-nos/src/test/java/pt/nos/test/TestApiNosApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.nos.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApiNosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
